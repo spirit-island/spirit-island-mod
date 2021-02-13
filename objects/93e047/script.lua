631511a56be8b1c4bfa627149afa5e16bdcca32d
Jeff Dolan <>
2021-02-13 11:28:37 -0600

    Renamed Defence to Defend

--- objects/93e047/script.lua
+++ objects/93e047/script.lua
@@ -1,5 +1,5 @@
 function onCollisionEnter(collision_info)
-    if collision_info.collision_object.type == "Generic" and (collision_info.collision_object.getName() == "Defence" or string.match(collision_info.collision_object.getName(), "^%a*'s Defence")) then
+    if collision_info.collision_object.type == "Generic" and (collision_info.collision_object.getName() == "Defend" or string.match(collision_info.collision_object.getName(), "^%a*'s Defend")) then
         destroyObject(collision_info.collision_object)
         return
     end

0f64ed2a84dd6dfe66ae8f4369494446f18fb43d
iakona <1824122+iakona@users.noreply.github.com>
2021-03-14 17:06:35 -0500

    Fixed bug where elemental invocation always removed top invader card as opposed to lowest stage

--- objects/b8b521/script.lua
+++ objects/b8b521/script.lua
@@ -6,7 +6,31 @@ mapSetup=true
 broadcast="For proper element counting, place 2 element tokens instead of 1 on the corners"
 
 function InvaderDeckSetup(params)
-    table.remove(params.deck,1)
+    local found = false
+    local stageTwo = -1
+    local stageThree = -1
+    for i, stage in pairs(params.deck) do
+        if stage == 1 then
+            table.remove(params.deck,i)
+            found = true
+            break
+        elseif stage == 2 or stage == "C" then
+            if stageTwo == -1 then
+                stageTwo = i
+            end
+        elseif stage == 3 then
+            if stageThree == -1 then
+                stageThree = i
+            end
+        end
+    end
+    if not found then
+        if stageTwo ~= -1 then
+            table.remove(params.deck,stageTwo)
+        else
+            table.remove(params.deck,stageThree)
+        end
+    end
     return params.deck
 end
 

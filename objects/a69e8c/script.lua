24adb311606a641ee845b4506ea32e81934f6221
iakona <1824122+iakona@users.noreply.github.com>
2021-03-16 19:00:13 -0500

    Added new scenario token models (both B&C and JE) and updated the 4 scenarios to work with these new tokens

--- objects/a69e8c/script.lua
+++ objects/a69e8c/script.lua
@@ -5,11 +5,19 @@ postSetupComplete=false
 
 function PostSetup()
     local scenarioBag = Global.getVar("scenarioBag")
-    scenarioBag.takeObject({
-        guid = "0841e7",
+    local bag = scenarioBag.takeObject({
+        guid = "8d6e46",
         position = {-45.24, 0.84, 36.64},
         rotation = {0,180,0},
         smooth = false,
+        callback_function = removeTokens,
     })
+    bag.setLock(true)
+end
+function removeTokens(obj)
+    -- Remove 8 numbered tokens
+    for _ = 1, 8 do
+        obj.takeObject({}).destruct()
+    end
     postSetupComplete = true
 end

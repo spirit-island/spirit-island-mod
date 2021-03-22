7ffb31993e91beeb98ac8028fbd4dc708356f4b2
iakona <1824122+iakona@users.noreply.github.com>
2021-03-21 20:02:34 -0500

    Updated card assets to latest printing and making quality consistent across expansions

--- objects/7ac013/script.lua
+++ objects/7ac013/script.lua
@@ -7,12 +7,12 @@ function FearSetup(params)
     params.deck.takeObject({
         guid = "2b7d0b",
         position = {-46.18, 0.82, 35.58},
-        rotation = {0,180,0},
+        rotation = {0,180,180},
     })
     params.deck.takeObject({
         guid = "4211e9",
         position = {-41.70, 0.82, 35.58},
-        rotation = {0,180,0},
+        rotation = {0,180,180},
     })
     local fearDeck = getObjectFromGUID(Global.getVar("fearDeckSetupZone")).getObjects()[1]
     params.deck.putObject(fearDeck)

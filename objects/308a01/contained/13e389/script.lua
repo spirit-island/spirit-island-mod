917fb62d45b5cfb0048935a2da521d029d3cad75
iakona <1824122+iakona@users.noreply.github.com>
2021-09-05 18:01:33 -0500

    Added thresholds for base game major powers

--- objects/308a01/contained/13e389/script.lua
+++ objects/308a01/contained/13e389/script.lua
@@ -1,2 +1,9 @@
 elements="00100110"
 energy=5
+function onLoad(saved_data)
+    if saved_data ~= "" then
+        local loaded_data = JSON.decode(saved_data)
+        self.setTable("thresholds", loaded_data.thresholds)
+    end
+end
+-- card loading end

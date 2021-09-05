92b880fc3c73d9e06cadb626ba45cdb71afc1347
iakona <1824122+iakona@users.noreply.github.com>
2021-09-05 18:13:29 -0500

    Added thresholds for B&C major powers

--- objects/BnCBag/contained/07ac50/contained/119a7e/script.lua
+++ objects/BnCBag/contained/07ac50/contained/119a7e/script.lua
@@ -1,2 +1,9 @@
 elements="11000101"
 energy=6
+function onLoad(saved_data)
+    if saved_data ~= "" then
+        local loaded_data = JSON.decode(saved_data)
+        self.setTable("thresholds", loaded_data.thresholds)
+    end
+end
+-- card loading end

e42395058e949816863bfb131313a4f86923ad1d
iakona <1824122+iakona@users.noreply.github.com>
2021-09-05 18:56:16 -0500

    Added thresholds to JE major powers

--- objects/JEBag/contained/98a916/contained/9e71fe/script.lua
+++ objects/JEBag/contained/98a916/contained/9e71fe/script.lua
@@ -1,2 +1,9 @@
 elements="10010010"
 energy=6
+function onLoad(saved_data)
+    if saved_data ~= "" then
+        local loaded_data = JSON.decode(saved_data)
+        self.setTable("thresholds", loaded_data.thresholds)
+    end
+end
+-- card loading end
--- objects/JEBag/contained/98a916/contained/d45b0d/script.lua
+++ objects/JEBag/contained/98a916/contained/d45b0d/script.lua
@@ -1,2 +1,9 @@
 elements="10001101"
 energy=2
+function onLoad(saved_data)
+    if saved_data ~= "" then
+        local loaded_data = JSON.decode(saved_data)
+        self.setTable("thresholds", loaded_data.thresholds)
+    end
+end
+-- card loading end

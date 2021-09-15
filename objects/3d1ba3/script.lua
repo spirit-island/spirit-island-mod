1a88eefb3f3c3024cd2a98d93bc5400b5a78636f
iakona <1824122+iakona@users.noreply.github.com>
2021-09-15 16:43:40 -0500

    Added comment with link to a tts bug report

--- objects/3d1ba3/script.lua
+++ objects/3d1ba3/script.lua
@@ -4,6 +4,7 @@ postSetup = true
 postSetupComplete = false
 
 function PostSetup()
+    -- TODO fix me once bug is resolved https://tabletopsimulator.nolt.io/1272
     Hands.hiding = 1
     postSetupComplete = true
 end

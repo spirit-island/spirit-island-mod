01274bce88981b0c4f5fb70c7043d9bac2d4f735
iakona <1824122+iakona@users.noreply.github.com>
2021-10-21 21:24:44 -0500

    Thieves bag will now auto shuffle when you put a thief back into it

--- objects/ec49d4/script.lua
+++ objects/ec49d4/script.lua
@@ -3,10 +3,12 @@ difficulty=2
 postSetup=true
 postSetupComplete=false
 
+thievesBag="baeea1"
+
 function PostSetup()
     local scenarioBag = Global.getVar("scenarioBag")
     local bag = scenarioBag.takeObject({
-        guid = "baeea1",
+        guid = thievesBag,
         position = {-42.14, 0.71, 35.97},
         rotation = {0,0,0},
         smooth = false,
@@ -14,4 +16,10 @@ function PostSetup()
     })
     bag.shuffle()
     postSetupComplete = true
+end
+
+function onObjectEnterContainer(container, _)
+    if container.guid == thievesBag and Global.getVar("gameStarted") then
+        container.shuffle()
+    end
 end

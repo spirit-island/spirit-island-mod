92f59ca628325fc759167585eb45f16b1b606a8c
MJ <MJyiolate@gmail.com>
2021-10-26 17:52:58 +0100

    Updated models/colliders of Invaders and colours of Adversary-specific Invaders

--- objects/1d9bcd/script.lua
+++ objects/1d9bcd/script.lua
@@ -50,7 +50,7 @@ function PreSetup(params)
         townBag = adversaryBag.takeObject({
             guid = newGuid,
             position = townBagPosition,
-            rotation = {0,270,0},
+            rotation = {0,180,0},
             smooth = false,
             callback_function = function(obj) obj.setLock(true) end,
         })

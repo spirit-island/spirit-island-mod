6b2fa62ebba63fa5fcf62adc11c1687ad5027f9f
iakona <1824122+iakona@users.noreply.github.com>
2022-02-12 09:28:42 -0600

    Elemental Invocation Accelerate is no longer automated since card should go to discard not be removed

--- objects/b8b521/script.lua
+++ objects/b8b521/script.lua
@@ -2,8 +2,8 @@ difficulty=1
 fearCards={1,0,0}
 
 onObjectCollision=true
-invaderDeckSetup=true
 mapSetup=true
+hasBroadcast=true
 
 function onLoad()
     if Global.getVar("gameStarted") then
@@ -41,38 +41,13 @@ function onObjectCollisionExit(params)
     end
 end
 
-function InvaderDeckSetup(params)
-    local found = false
-    local stageTwo = -1
-    local stageThree = -1
-    for i, stage in pairs(params.deck) do
-        if stage == 1 then
-            table.remove(params.deck,i)
-            found = true
-            break
-        elseif stage == 2 or stage == "C" then
-            if stageTwo == -1 then
-                stageTwo = i
-            end
-        elseif stage == 3 then
-            if stageThree == -1 then
-                stageThree = i
-            end
-        end
-    end
-    if not found then
-        if stageTwo ~= -1 then
-            table.remove(params.deck,stageTwo)
-        else
-            table.remove(params.deck,stageThree)
-        end
-    end
-    return params.deck
-end
-
 function MapSetup(params)
     if not params.extra then
         table.insert(params.pieces[6],"Town")
     end
     return params.pieces
+end
+
+function Broadcast(params)
+    return "Elemental Invocation - Before Setup don't forget to Accelerate the Invader Deck."
 end

d73ab87b3b50d10d9af0308252014266ed41d006
iakona <1824122+iakona@users.noreply.github.com>
2022-04-25 13:08:39 -0500

    3.3.1 Release

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 ---- Versioning
-version = "3.3.0"
+version = "3.3.1"
 versionGuid = "57d9fe"
 ---- Used with Spirit Board Scripts
 counterBag = "EnergyCounters"
@@ -3473,8 +3473,9 @@ function resetPiece(object, rotation, offset, skip)
     end
 
     if upsideDown then
+        local flipOffset = object.getBounds().offset.y*2
         object.setRotation(rotation)
-        object.setPosition(object.getPosition() - Vector(0,object.getBounds().size.y,0))
+        object.setPosition(object.getPosition() - Vector(0,flipOffset,0))
     elseif not skip and object.getStateId() ~= -1 and object.getStateId() ~= 1 then
         object.setRotation(rotation)
         object.setPositionSmooth(object.getPosition() + Vector(0,objOffset,0))

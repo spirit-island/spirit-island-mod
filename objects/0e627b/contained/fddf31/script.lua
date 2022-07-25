41e90af49973fd3fb136bb33ef347cf67f9c421c
iakona <1824122+iakona@users.noreply.github.com>
2022-07-25 14:27:50 -0500

    Fractured will now create a 3rd hand

--- objects/0e627b/contained/fddf31/script.lua
+++ objects/0e627b/contained/fddf31/script.lua
@@ -9,53 +9,70 @@ function doSetup(params)
     end
 
     local handZone = Player[color].getHandTransform(2)
-    local newHandZone = {
+    handZone.position.z = handZone.position.z - 5.5
+    spawnObjectData({
+        data = {
+            Name = "HandTrigger",
+            FogColor = color,
+            Transform = {
+                posX = 0,
+                posY = 0,
+                posZ = 0,
+                rotX = 0,
+                rotY = 0,
+                rotZ = 0,
+                scaleX = 1.0,
+                scaleY = 1.0,
+                scaleZ = 1.0
+            },
+            Locked = true,
+        },
         position = handZone.position,
         rotation = handZone.rotation,
         scale = handZone.scale,
-    }
-    newHandZone.position.z = newHandZone.position.z - 5.5
-    Player[color].setHandTransform(newHandZone, 3)
+    })
 
-    local count = 4
-    if Global.getVar("numPlayers") <= 2 then
-        count = 6
-    end
-    if Global.getVar("numBoards") == 1 then
-        Player[color].broadcast("Don't forget to gain 1 Time", "Blue")
-    end
-
-    local playtestPowers = Global.getVar("playtestMinorPowers")
-    local playtestCount = playtestPowers
-    if count == 6 then
-        if playtestPowers == 1 then
-            playtestCount = 2
-        elseif playtestPowers == 2 then
-            playtestCount = 3
+    Wait.frames(function()
+        local count = 4
+        if Global.getVar("numPlayers") <= 2 then
+            count = 6
         end
-    end
-    local minorPowerDeck = getObjectFromGUID(Global.getVar("minorPowerZone")).getObjects()[1]
-    minorPowerDeck.deal(count - playtestCount, color, 3)
-    if playtestCount > 0 then
-        minorPowerDeck = getObjectFromGUID(Global.getVar("playtestMinorPowerZone")).getObjects()[1]
-        minorPowerDeck.deal(playtestCount, color, 3)
-    end
-
-    playtestPowers = Global.getVar("playtestMajorPowers")
-    playtestCount = playtestPowers
-    if count == 6 then
-        if playtestPowers == 1 then
-            playtestCount = 2
-        elseif playtestPowers == 2 then
-            playtestCount = 3
+        if Global.getVar("numBoards") == 1 then
+            Player[color].broadcast("Don't forget to gain 1 Time", "Blue")
         end
-    end
-    local majorPowerDeck = getObjectFromGUID(Global.getVar("majorPowerZone")).getObjects()[1]
-    majorPowerDeck.deal(count - playtestCount, color, 3)
-    if playtestCount > 0 then
-        majorPowerDeck = getObjectFromGUID(Global.getVar("playtestMajorPowerZone")).getObjects()[1]
-        majorPowerDeck.deal(playtestCount, color, 3)
-    end
 
-    self.destruct()
+        local playtestPowers = Global.getVar("playtestMinorPowers")
+        local playtestCount = playtestPowers
+        if count == 6 then
+            if playtestPowers == 1 then
+                playtestCount = 2
+            elseif playtestPowers == 2 then
+                playtestCount = 3
+            end
+        end
+        local minorPowerDeck = getObjectFromGUID(Global.getVar("minorPowerZone")).getObjects()[1]
+        minorPowerDeck.deal(count - playtestCount, color, 3)
+        if playtestCount > 0 then
+            minorPowerDeck = getObjectFromGUID(Global.getVar("playtestMinorPowerZone")).getObjects()[1]
+            minorPowerDeck.deal(playtestCount, color, 3)
+        end
+
+        playtestPowers = Global.getVar("playtestMajorPowers")
+        playtestCount = playtestPowers
+        if count == 6 then
+            if playtestPowers == 1 then
+                playtestCount = 2
+            elseif playtestPowers == 2 then
+                playtestCount = 3
+            end
+        end
+        local majorPowerDeck = getObjectFromGUID(Global.getVar("majorPowerZone")).getObjects()[1]
+        majorPowerDeck.deal(count - playtestCount, color, 3)
+        if playtestCount > 0 then
+            majorPowerDeck = getObjectFromGUID(Global.getVar("playtestMajorPowerZone")).getObjects()[1]
+            majorPowerDeck.deal(playtestCount, color, 3)
+        end
+
+        self.destruct()
+    end, 1)
 end

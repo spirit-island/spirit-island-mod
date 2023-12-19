e0e314cf9ff1f680136ee52d17c28a976c2377b8
iakona <1824122+iakona@users.noreply.github.com>
2023-12-19 16:10:18 -0600

    Fixed scripting error when putting hostility or immense back into bag. Also cleaned up some extraneous script state data

--- objects/f7f008/contained/e738ed/script.lua
+++ objects/f7f008/contained/e738ed/script.lua
@@ -1,33 +1,36 @@
 spiritName = "Keeper of the Forbidden Wilds"
 
 function doSetup(params)
-    local color = params.color
     local keeper = Global.call("getSpirit", {name = spiritName})
-    local json = JSON.decode(keeper.script_state)
-    if not json.hostility then
-        Global.call("giveEnergy", {color=color, energy=1, ignoreDebt=false})
-        for _,data in pairs(json.trackEnergy) do
-            data.count = math.ceil(data.count / 2)
+    if keeper then
+        local json = JSON.decode(keeper.script_state)
+        if not json.hostility then
+            Global.call("giveEnergy", {color=params.color, energy=1, ignoreDebt=false})
+            for _,data in pairs(json.trackEnergy) do
+                data.count = math.ceil(data.count / 2)
+            end
+            json.hostility = true
+            keeper.script_state = JSON.encode(json)
+            keeper.setTable("trackEnergy", json.trackEnergy)
         end
-        json.hostility = true
-        keeper.script_state = JSON.encode(json)
-        keeper.setTable("trackEnergy", json.trackEnergy)
     end
     return true
 end
 
 function onDestroy()
     local keeper = Global.call("getSpirit", {name = spiritName})
-    local json = JSON.decode(keeper.script_state)
-    if json.hostility then
-        for i,data in pairs(json.trackEnergy) do
-            data.count = data.count * 2
-            if i == 1 or i == 3 or i == 4 then
-                data.count = data.count + 1
+    if keeper then
+        local json = JSON.decode(keeper.script_state)
+        if json.hostility then
+            for i,data in pairs(json.trackEnergy) do
+                data.count = data.count * 2
+                if i == 1 or i == 3 or i == 4 then
+                    data.count = data.count + 1
+                end
             end
+            json.hostility = nil
+            keeper.script_state = JSON.encode(json)
+            keeper.setTable("trackEnergy", json.trackEnergy)
         end
-        json.hostility = nil
-        keeper.script_state = JSON.encode(json)
-        keeper.setTable("trackEnergy", json.trackEnergy)
     end
 end

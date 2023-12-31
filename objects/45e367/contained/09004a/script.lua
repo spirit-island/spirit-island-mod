84058a3963cd57df05339516841fefea1c26eea0
iakona <1824122+iakona@users.noreply.github.com>
2023-12-31 15:10:38 -0600

    Ported over darkfire element and threshold modifying code

--- objects/45e367/contained/09004a/script.lua
+++ objects/45e367/contained/09004a/script.lua
@@ -1,4 +1,18 @@
 spiritName = "Shadows Flicker Like Flame"
+elements={0,0,1,0,0,0,0,0}
+
+function modifyElements(params)
+    if params.color == Global.call("getSpiritColor", {name = spiritName}) then
+        local darkFire = params.elements[2] + params.elements[3]
+        params.elements[2] = darkFire
+        params.elements[3] = darkFire
+    end
+    return params.elements
+end
+
+function modifyThresholds(params)
+    return modifyElements({color = params.color, elements = params.elements})
+end
 
 function doSetup(params)
     local color = params.color

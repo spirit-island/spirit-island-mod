ec70dd6cf4e58acee50e8b28676a406c22bd904a
iakona <1824122+iakona@users.noreply.github.com>
2023-12-31 16:02:41 -0600

    Added scripting zone over ravage area so salt card can detect when it enters the zone and automatically lock

--- objects/AdversaryBag/contained/8dc86e/script.lua
+++ objects/AdversaryBag/contained/8dc86e/script.lua
@@ -1,2 +1,13 @@
 cardInvaderType="L"
 cardInvaderStage=2
+
+function onObjectEnterZone(zone, object)
+    if object ~= self then return end
+    -- automatically lock the card when it enters the ravage zone
+    local ravageZoneGUID = "2403e9"
+    if zone.getGUID() == ravageZoneGUID then
+        print("locking "..object.getName())
+        -- a small wait, to allow it to settle onto the board before being locked
+        Wait.time(function() self.setLock(true) end, 1)
+    end
+end
